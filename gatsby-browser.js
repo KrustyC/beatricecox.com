4aaa7044cc2f8a54a20dbb6d519ac218d4a93daa
Davide Crestini <davide.crestini94@gmail.com>
2019-07-08 09:25:08 +0100

    fixed issue with redux

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,10 +1,2 @@
-import React from 'react';
-import { Provider } from 'react-redux';
-
-import createStore from './src/redux/createStore';
-
-// eslint-disable-next-line import/prefer-default-export,react/prop-types
-export const wrapRootElement = ({ element }) => {
-  const store = createStore();
-  return <Provider store={store}>{element}</Provider>;
-};
+// eslint-disable-next-line import/prefer-default-export
+export { default as wrapRootElement } from './wrap-with-provider';
--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,7 +1,2 @@
-/**
- * Implement Gatsby's SSR (Server Side Rendering) APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/ssr-apis/
- */
-
-// You can delete this file if you're not using it
+// eslint-disable-next-line import/prefer-default-export
+export { default as wrapRootElement } from './wrap-with-provider';
