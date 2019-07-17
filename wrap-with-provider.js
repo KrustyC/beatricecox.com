3caeac07e4fe61b0e1c5b0c26a92a6e081aadfcd
Davide <davide.crestini94@gmail.com>
2019-07-18 00:02:34 +0100

    fixed linting as well

--- wrap-with-provider.js
+++ wrap-with-provider.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Provider } from 'react-redux';
 
-import GlobalLayout from './src/components/GlobalLayout';
 import createStore from './src/state/createStore';
 
 // eslint-disable-next-line react/prop-types
