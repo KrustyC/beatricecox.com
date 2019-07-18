66986cbd26ff5e5b0524fbc099d67032bea42b42
Davide Crestini <davide.crestini94@gmail.com>
2019-07-18 13:34:00 +0100

    added project page

--- src/pages/projects/_Item.js
+++ src/pages/projects/_Item.js
@@ -3,13 +3,27 @@ import PropTypes from 'prop-types';
 import styled from 'styled-components';
 import { Link } from 'gatsby';
 
-const Wrapper = styled(Link)``;
+const Wrapper = styled(Link)`
+  text-decoration: none;
+  background-image: url('${({ url }) => url}');
+  background-repeat: no-repeat;
+  background-size: 100% 100%;
+  display: flex;
+  align-items: flex-start;
+  justify-content: flex-end;
+  padding-top: 100px;
+  padding-right: 10px;
+  height: 500px;
+`;
+const Number = styled.span`
+  font-size: 50px;
+  transform: rotate(-90deg);
+  color: #000;
+`;
 
-const Item = ({ id, data }) => (
-  <Wrapper to={`/portfolio/${id}`}>
-    <h1>{data.title.text}</h1>
-    <p>{data.project_description.text}</p>
-    <p>{data.project_description.text}</p>
+const Item = ({ id, data, order }) => (
+  <Wrapper to={`/portfolio/${id}`} url={data.project_picture.url}>
+    <Number>{order < 10 ? `0${order}` : order}</Number>
   </Wrapper>
 );
 
@@ -26,6 +40,7 @@ Item.propTypes = {
       url: PropTypes.string.isRequired,
     }).isRequired,
   }).isRequired,
+  order: PropTypes.number.isRequired,
 };
 
 export default Item;
--- src/pages/projects/index.js
+++ src/pages/projects/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { graphql } from 'gatsby';
 import styled from 'styled-components';
+import Links from '../../components/Links';
 
 import SEO from '../../components/Seo';
 import Item from './_Item';
@@ -10,20 +11,16 @@ const Grid = styled.div`
   min-height: 100vh;
   display: grid;
   color: black;
-  grid-template-columns: repeat(8, 1fr);
-  grid-template-areas:
-    '. header header header header header header header'
-    'main main main main main main main hero-side'
-    'main main main main main main main hero-side'
-    'main main main main main main main hero-side'
-    'main main main main main main main hero-side'
-    'main main main main main main main hero-side'
-    'main main main main main main main hero-side';
+  grid-template-columns: 1fr 3fr 1fr;
+  grid-template-rows: 100px auto 200px;
 `;
 
 const Header = styled.div`
   text-transform: uppercase;
-  grid-area: header;
+  grid-column-start: 2;
+  grid-column-end: end;
+  grid-row-start: 1;
+  grid-row-end: 1;
 
   display: flex;
   align-items: center;
@@ -36,6 +33,163 @@ const Header = styled.div`
   }
 `;
 
+const Main = styled.div`
+  grid-column-start: 2;
+  grid-column-end: 3;
+  grid-row-start: 2;
+
+  display: grid;
+  grid-column-gap: 10px;
+  grid-row-gap: 10px;
+  grid-template-columns: 1fr 1fr;
+`;
+
+const MainSide = styled.div`
+  grid-column-start: 3;
+  grid-column-end: end;
+  grid-row-start: 2;
+  grid-row-end: end;
+  padding-left: 50px;
+`;
+
+const Footer = styled.div`
+  text-transform: uppercase;
+  grid-column-start: 2;
+  grid-column-end: 3;
+  grid-row-start: 3;
+
+  padding-bottom: 20px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  span:first-of-type {
+    padding-bottom: 20px;
+  }
+`;
+
+const fakeData = [
+  {
+    id: 1,
+    data: {
+      title: {
+        text: 'babington',
+      },
+      project_description: {
+        text: 'Whatever',
+      },
+      project_picture: {
+        url:
+          'https://images.unsplash.com/photo-1535498730771-e735b998cd64?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80',
+      },
+    },
+  },
+  {
+    id: 2,
+    data: {
+      title: {
+        text: 'babington',
+      },
+      project_description: {
+        text: 'Whatever',
+      },
+      project_picture: {
+        url:
+          'https://images.unsplash.com/photo-1535498730771-e735b998cd64?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80',
+      },
+    },
+  },
+  {
+    id: 3,
+    data: {
+      title: {
+        text: 'babington',
+      },
+      project_description: {
+        text: 'Whatever',
+      },
+      project_picture: {
+        url:
+          'https://images.unsplash.com/photo-1535498730771-e735b998cd64?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80',
+      },
+    },
+  },
+  {
+    id: 4,
+    data: {
+      title: {
+        text: 'babington',
+      },
+      project_description: {
+        text: 'Whatever',
+      },
+      project_picture: {
+        url:
+          'https://images.unsplash.com/photo-1535498730771-e735b998cd64?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80',
+      },
+    },
+  },
+  {
+    id: 5,
+    data: {
+      title: {
+        text: 'babington',
+      },
+      project_description: {
+        text: 'Whatever',
+      },
+      project_picture: {
+        url:
+          'https://images.unsplash.com/photo-1535498730771-e735b998cd64?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80',
+      },
+    },
+  },
+  {
+    id: 6,
+    data: {
+      title: {
+        text: 'babington',
+      },
+      project_description: {
+        text: 'Whatever',
+      },
+      project_picture: {
+        url:
+          'https://images.unsplash.com/photo-1535498730771-e735b998cd64?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80',
+      },
+    },
+  },
+  {
+    id: 7,
+    data: {
+      title: {
+        text: 'babington',
+      },
+      project_description: {
+        text: 'Whatever',
+      },
+      project_picture: {
+        url:
+          'https://images.unsplash.com/photo-1535498730771-e735b998cd64?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80',
+      },
+    },
+  },
+  {
+    id: 8,
+    data: {
+      title: {
+        text: 'babington',
+      },
+      project_description: {
+        text: 'Whatever',
+      },
+      project_picture: {
+        url:
+          'https://images.unsplash.com/photo-1535498730771-e735b998cd64?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80',
+      },
+    },
+  },
+];
+
 const Portfolio = ({
   data: {
     allPrismicPortfolioItem: { nodes },
@@ -47,15 +201,19 @@ const Portfolio = ({
       <Header>
         <h1>What?</h1>
       </Header>
+      <Main>
+        {fakeData.map(({ id, data }, i) => (
+          <Item key={id} data={data} order={i + 1} />
+        ))}
+      </Main>
+      <MainSide>
+        <Links />
+      </MainSide>
+      <Footer>
+        <span>Download CV</span>
+        <span>Download Portfolio</span>
+      </Footer>
     </Grid>
-    {/* <h1>Portfolio</h1>
-    <p>This is a list of all the amazing stuff {`I've`} done during my life</p>
-    <p>Now go build something great.</p>
-    <Grid>
-      {nodes.map(({ id, data }) => (
-        <Item key={id} data={data} />
-      ))}
-    </Grid> */}
   </>
 );
 
