d91a6cfd476e73c76698b49ffcf05f614959d84f
Davide <davide.crestini94@gmail.com>
2019-08-06 11:53:06 +0200

    fixed extras grid

--- src/pages/extras/index.js
+++ src/pages/extras/index.js
@@ -1,45 +1,58 @@
 import React, { useEffect, useState } from 'react';
 import styled from 'styled-components';
 import media from 'styled-media-query';
+import Masonry from 'react-masonry-css';
 
 import SEO from '../../components/Seo';
 import Layout from '../../components/Layout';
 
+const BASE_URL =
+  'https://api.flickr.com/services/rest/?method=flickr.people.getPhotos';
 const FLICKR_USER_ID = '147991560@N07';
-const FLICKR_URL = `https://api.flickr.com/services/rest/?method=flickr.people.getPhotos&api_key=${process.env.FLICKR_API_KEY}&user_id=${FLICKR_USER_ID}&format=json&nojsoncallback=?`;
+const FLICKR_URL = `${BASE_URL}&api_key=${process.env.FLICKR_API_KEY}&user_id=${FLICKR_USER_ID}&format=json&nojsoncallback=?`;
 
 const Main = styled.div`
   grid-column: 2 / 3;
   grid-row: 2 / end;
-  display: grid;
-  grid-template-columns: 1fr 1fr;
-  grid-column-gap: 10px;
-  grid-row-gap: 10px;
 
   ${media.lessThan('medium')`
     grid-column: 1 / end;
     grid-template-columns: 1fr;
     padding: 0 .5em;
   `}
+
+  .my-masonry-grid {
+    display: flex;
+    margin-left: -10px;
+    width: 100%;
+
+    ${media.lessThan('medium')`
+      margin-left: 0;
+    `}
+  }
+  .my-masonry-grid_column {
+    padding-left: 10px;
+    background-clip: padding-box;
+
+    img {
+      height: auto;
+      width: 100%;
+      margin-bottom: 5px;
+    }
+
+    ${media.lessThan('medium')`
+      padding-left: 0;
+    `}
+  }
 `;
 
-const Grid = styled.div`
-  display: flex;
-  flex: 1;
-  flex-direction: column;
-`;
-
-const fetchFlickPhotos = () => fetch(FLICKR_URL).then(res => res.json());
+const fetchFlickrPhotos = () => fetch(FLICKR_URL).then(res => res.json());
 
 const Extras = () => {
-  const [photos1, setPhotos1] = useState([]);
-  const [photos2, setPhotos2] = useState([]);
+  const [photos, setPhotos] = useState([]);
 
   useEffect(() => {
-    fetchFlickPhotos().then(res => {
-      setPhotos1(res.photos.photo.slice(0, res.photos.photo.length / 2));
-      setPhotos2(res.photos.photo.slice(res.photos.photo.length / 2));
-    });
+    fetchFlickrPhotos().then(res => setPhotos(res.photos.photo));
   }, []);
 
   return (
@@ -47,28 +60,23 @@ const Extras = () => {
       <SEO title="Extras" />
       <Layout title="Why?">
         <Main>
-          <Grid>
-            {photos1.map(photo => {
+          <Masonry
+            breakpointCols={{
+              default: 2,
+              1100: 2,
+              700: 2,
+              500: 1,
+            }}
+            className="my-masonry-grid"
+            columnClassName="my-masonry-grid_column"
+          >
+            {photos.map(photo => {
               const { farm, server, id, secret } = photo;
               const link = `https://farm${farm}.staticflickr.com/${server}/${id}_${secret}.jpg`;
               return <img key={id} src={link} alt="" />;
             })}
-          </Grid>
-          <Grid>
-            {photos2.map(photo => {
-              const { farm, server, id, secret } = photo;
-              const link = `https://farm${farm}.staticflickr.com/${server}/${id}_${secret}.jpg`;
-              return <img key={id} src={link} alt="" />;
-            })}
-          </Grid>
+          </Masonry>
         </Main>
-        {/* <Grid>
-          {photos.map(photo => {
-            const { farm, server, id, secret } = photo;
-            const link = `https://farm${farm}.staticflickr.com/${server}/${id}_${secret}.jpg`;
-            return <img src={link} />;
-          })}
-        </Grid> */}
       </Layout>
     </>
   );
