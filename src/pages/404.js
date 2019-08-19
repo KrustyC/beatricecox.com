862aa2918ba8e0f7e542520ca0004fa438b7d804
Davide Crestini <davide.crestini94@gmail.com>
2019-08-19 14:37:19 +0100

    improved 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,13 +1,33 @@
 import React from 'react';
-
+import styled from 'styled-components';
+import { Link } from 'gatsby';
 import SEO from '../components/Seo';
 
+const Div = styled.div`
+  height: 100vh;
+  width: 100vw;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+
+  p {
+    text-align: center;
+    * {
+      font-family: 'Montserrat' !important;
+    }
+  }
+`;
 const NotFoundPage = () => (
-  <React.Fragment>
+  <Div>
     <SEO title="404: Not found" />
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-  </React.Fragment>
+    <h1>PAGE NOT FOUND</h1>
+    <p>
+      The page you are looking for does not exists on this website.
+      <br />
+      Please <Link to="/">click here</Link> to go back tothe home page.
+    </p>
+  </Div>
 );
 
 export default NotFoundPage;
