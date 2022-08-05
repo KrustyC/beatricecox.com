77a5397f8e38f54325b0a07dcd70f3fb8bfd1567
Davide Crestini <davide.crestini94@gmail.com>
2022-08-05 18:14:27 +0100

    feat: added authentication

---
+++ src/middleware.ts
@@ -0,0 +1,28 @@
+import type { NextRequest } from "next/server";
+import { NextResponse } from "next/server";
+
+const BASIC_AUTH_USER = process.env.BASIC_AUTH_USER;
+const BASIC_AUTH_PASS = process.env.BASIC_AUTH_PASS;
+
+export const config = {
+  matcher: "/:path*",
+};
+
+export const middleware = (req: NextRequest) => {
+  const basicAuth = req.headers.get("authorization");
+
+  console.log(basicAuth);
+  if (basicAuth) {
+    const auth = basicAuth.split(" ")[1];
+
+    const [user, password] = atob(auth).split(":");
+
+    if (user === BASIC_AUTH_USER && password === BASIC_AUTH_PASS) {
+      return NextResponse.next();
+    }
+  }
+
+  const url = req.nextUrl.clone();
+  url.pathname = "/api/auth";
+  return NextResponse.rewrite(url);
+};
