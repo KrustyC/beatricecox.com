3ac14854437a111ab3800e6e231c6931564f899f
Davide Crestini <davidecrestini@Davides-MacBook-Air.local>
2023-02-14 11:12:06 +0700

    fix: gif

--- src/components/Home/Hero.tsx
+++ src/components/Home/Hero.tsx
@@ -14,6 +14,7 @@ export const Hero = () => (
             fill
             loading="lazy"
             sizes="100vw"
+            unoptimized
             style={{
               objectFit: "contain",
             }}
