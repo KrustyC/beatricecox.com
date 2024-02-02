896020ba3cb3673c1fc694b18d9059ddbd128643
Davide Crestini <davide.crestini94@gmail.com>
2024-02-02 19:02:04 +0000

    fix: prettier

--- src/app/projects/[slug]/page.tsx
+++ src/app/projects/[slug]/page.tsx
@@ -1,6 +1,6 @@
 import { Metadata } from "next";
 import { draftMode } from "next/headers";
-import { notFound,redirect } from "next/navigation";
+import { notFound, redirect } from "next/navigation";
 
 import { Project } from "@/components/Project";
 import { getProject } from "@/graphql/queries/get-project.query";
--- src/hooks/useOnClickOutside.ts
+++ src/hooks/useOnClickOutside.ts
@@ -1,4 +1,4 @@
-import { RefObject,useEffect } from "react";
+import { RefObject, useEffect } from "react";
 
 type Event = MouseEvent | TouchEvent;
 
