696b555db40f3d40d4b48cf3c9c00ea5bf04773f
Davide Crestini <davide.crestini94@gmail.com>
2025-04-25 21:31:31 -0600

    chore: updated favicons and error page

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -1,13 +1,14 @@
 "use client";
 
+import { ChickenImage } from "@/components/ChickenImage";
+
 export default function GenericError() {
   return (
-    <div className="flex flex-col items-center w-full py-12">
-      {/* <BugFixingIcon className="h-[180px] w-[320px] lg:h-[480px] lg:w-[620px]" /> */}
+    <div className="flex flex-col justify-center items-center w-full min-h-[90vh] py-[120px]">
+      <ChickenImage />
 
-      <h2 className="text-2xl lg:text-4xl w-[320px] lg:w-[620px] mt-8">
-        An error happened. Our devs have been informed and are already working
-        towards a fix. Please try again soon.
+      <h2 className="text-3xl text-center w-[320px] lg:w-[620px] mt-8">
+        Oops, this was unexpected. <br /> Please try again later.
       </h2>
     </div>
   );
